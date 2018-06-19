cad6e02f623005eae394ff2ce80c111e7c660f7b
Maxim Kon0pelka <tkach32max@gmail.com>
2018-06-19 10:37:23 +0300

    some change and add method best answer

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,24 +1,36 @@
 class Api::V1::QuestionsController < ApplicationController
   before_action :set_question, only: [:show]
+  before_action :authenticate_user, only: [:create, :best_answer]
 
   def show
-    render json: @question
+    render json: @question, includes: :answers
   end
 
   def index
-    @questions = Question.page(params[:page]).per(params[:per_page]).includes(:author)
-    render json: @questions
+    questions = Question.page(params[:page]).per(10).includes(:autor)
+    render json: questions, include: :autor
   end
 
   def create
-    @categorys = Category.all
-    @question = current_user.question.build(question_params)
-    if @question.save
-      #Questionsz_categorie.create!( )
-      render json: @question, status: :created, location: api_v1_post_url(@question)
+    categ = params[:question][:categories]
+    question = current_user.questions.build(question_params)
+    question.categories = Category.find(categ)
+    if question.save
+      render json: question, status: :created, location: api_v1_question_url(question)
     else
-      render json: { errors: @question.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: question.errors.full_messages }, status: :unprocessable_entity
     end   
+  end
+
+  def best_answer
+    question = current_user.questions.find(params[:question_id])
+    answer = question.answers.find(params[:answer_id])
+    if answer
+      question.close_question(answer)
+      render json: question, status: :accepted, location: api_v1_question_url(question)
+    else
+    render json: question, status: :unprocessable_entity, location: api_v1_question_url(question)
+    end
   end
 
   private 
