7635481b93c6b9ddb1eadb4f56be47f77c93b02f
Maxim Kon0pelka <tkach32max@gmail.com>
2018-06-19 10:41:22 +0300

    add question relations and validates

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,15 @@
 class Question < ApplicationRecord
+    has_many :questions_categories
+    has_many :categories, through: :questions_categories
+    belongs_to :autor, class_name: 'User'
+    has_many :answers
+    belongs_to :answer, foreign_key: "answer_id", optional: true
+    default_scope -> { order(created_at: :desc) }
+    validates :title, presence: true, length: { maximum: 64 }
+    validates :info, presence: true
+    enum status: [:open, :close]
+
+    def close_question(answer)
+        update_attributes(answer: answer,status: :close)
+    end
 end
